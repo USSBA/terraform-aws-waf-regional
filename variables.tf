97b84ca64ecfcaf60226b9de5ff462bb039d56e8
Jason Smigovsky <jsmigovsky@fearless.tech>
2020-02-19 11:19:20 -0500

    IA-1285: add rate based rule

--- variables.tf
+++ variables.tf
@@ -321,3 +321,26 @@ variable "rule_country_of_origin_blacklist" {
   description = "A blacklist of countries."
   default     = ["CN", "HK"]
 }
+
+# rate limiting
+variable "rule_rate_limit" {
+  type        = string
+  description = "COUNT or BLOCK, any other value will disable this rule entirely."
+  default     = "DISABLED"
+}
+variable "rule_rate_limit_priority" {
+  type        = number
+  description = "The priority in which to execute this rule."
+  default     = 5
+}
+variable "rule_rate_limit_count" {
+  type        = number
+  description = "The number requests allowed over a 5 minute period (minimum value of 100 is enforced)"
+  default     = 100
+}
+variable "rule_rate_limit_paths" {
+  type        = list(string)
+  description = "A list of relative URL paths to rate limit"
+  default     = []
+}
+
